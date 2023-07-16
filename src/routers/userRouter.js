e8906b470facb75b860e794b423c3887125914c0
성환 <psung9510@gmail.com>
2023-07-16 15:07:23 +0900

    :white_check_mark: Handle multimedia data

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,18 +1,31 @@
 import express from "express";
 import {
-  edit,
+  getEdit,
+  postEdit,
   see,
   logout,
   startGithubLogin,
   endGithubLogin,
+  getChangePw,
+  postChangePw,
 } from "../controllers/userController";
+import {
+  multerMiddlewareAvatar,
+  protectorMiddleware,
+  publicOnlyMiddleware,
+} from "../middlewares";
 
 const userRouter = express.Router();
 
-userRouter.route("/:id([0-9a-f]{24})").get(see);
-userRouter.route("/logout").get(logout);
-userRouter.route("/edit").get(edit);
-userRouter.route("/github/start").get(startGithubLogin);
-userRouter.route("/github/callback").get(endGithubLogin);
+userRouter.route("/:id([0-9a-f]{24})").get(protectorMiddleware, see);
+userRouter.route("/logout").get(protectorMiddleware, logout);
+userRouter
+  .route("/edit")
+  .all(protectorMiddleware)
+  .get(getEdit)
+  .post(multerMiddlewareAvatar.single("avatar"), postEdit);
+userRouter.route("/github/start").get(publicOnlyMiddleware, startGithubLogin);
+userRouter.route("/github/callback").get(publicOnlyMiddleware, endGithubLogin);
+userRouter.route("/change-pw").get(getChangePw).post(postChangePw);
 
 export default userRouter;
